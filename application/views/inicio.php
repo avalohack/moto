07579fd3e0a70f56c93e1b6e40f4492276bc6066
Avalohack <info@vacasenvuelo.com>
2018-03-22 13:06:55 -0500

    aplicacion

---
+++ application/views/inicio.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Partes</title>
+	<link rel="stylesheet" type="text/css" href="vendor/css/css.css">
+</head>
+<body>
+
+
+
+
+<div class="container">
+  <div class="box-1">
+    box-1
+  <div>
+  <div class="box-2">
+    box-2
+  <div>
+  <div class="box-3">
+    box-3
+  <div>
+  <div class="box-4">
+    box-4
+  <div>
+  <div class="box-5">
+    box-5
+  <div>
+<div>
+
+
+-----------------------------------------
+
+
+	
+<div id="head"></div>
+<div id="tabla">
+	<table cellpadding="0" cellspacing="0" width="100%">
+		<thead>
+			<tr>
+				<th>Pieza</th>
+				<th>buscada</th>
+				<th>Le sirve </th>
+				<th>Recomendaciones</th>
+				<th>Colaborador</th>	
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>corona 18 bobinas</td>
+				<td>duke 200 ktm</td>
+				<td>gn 125 suzuki</td>
+				<td>se le retira la bobina captora, en la duke 2015 confirmado que funciona</td>
+				<td>avalo</td>
+			</tr>
+			<tr>
+				<td>corona 18 bobinas</td>
+				<td>duke 200 ktm</td>
+				<td>gn 125 suzuki</td>
+				<td>se le retira la bobina captora, en la duke 2015 confirmado que funciona</td>
+				<td>avalo</td>
+			</tr>
+			<tr>
+				<td>corona 18 bobinas</td>
+				<td>duke 200 ktm</td>
+				<td>gn 125 suzuki</td>
+				<td>se le retira la bobina captora, en la duke 2015 confirmado que funciona</td>
+				<td>avalo</td>
+			</tr>
+			<tr>
+				<td>corona 18 bobinas</td>
+				<td>duke 200 ktm</td>
+				<td>gn 125 suzuki</td>
+				<td>se le retira la bobina captora, en la duke 2015 confirmado que funciona</td>
+				<td>avalo</td>
+			</tr>
+			<tr>
+				<td>corona 18 bobinas</td>
+				<td>duke 200 ktm</td>
+				<td>gn 125 suzuki</td>
+				<td>se le retira la bobina captora, en la duke 2015 confirmado que funciona</td>
+				<td>avalo</td>
+			</tr>
+		</tbody>
+	</table>	
+</div>
+	
+
+</body>
+</html>
